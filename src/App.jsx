57a93189685d8dfae0bcf96b9dfe1d34d94eb902
Shubham Rao <shubhamrao834@gmail.com>
2024-08-25 20:25:02 +0530

    install react, tailwind and setup git repository

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      Hello
+      <p className="text-red-700">This is delote training project</p>
+    </div>
+  );
+}
+
+export default App;
